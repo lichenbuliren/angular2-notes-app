d32657fd85cb3d11577fe18827e4df84ee4757e7
lichenbuliren <617106268@qq.com>
2016-11-02 21:26:25 +0800

    重构 reduer 方法，解耦 switch 判断

--- src/app/app-redux/note-action-creators.ts
+++ src/app/app-redux/note-action-creators.ts
@@ -1,52 +1,42 @@
 import { Action, ActionCreator } from 'redux';
 import { Note } from '../note';
 
+export const ADD_NOTE: string = 'ADD_NOTE';
+export const DEL_NOTE: string = 'DEL_NOTE';
+export const UPDATE_ACTIVE_NOTE: string = 'UPDATE_ACTIVE_NOTE';
+export const TOGGLE_SHOW: string = 'TOGGLE_SHOW';
+export const TOGGLE_NOTE_FAVORITE: string = 'TOGGLE_NOTE_FAVORITE';
+
 /**
  * Redux 中的 action 只是一个接口， 只有一个 type 类型的字段，
  * 在这里我们需要自己扩展我们所需要的东西
  */
-
-export interface AddNoteAction extends Action {
-  note: Note
+export interface CommonStateAction extends Action {
+  note: Note,
+  show: string,
+  id: number
 }
-export const ADD_NOTE: string = 'ADD_NOTE';
+
 export const addNote: ActionCreator<Action> = (note: Note) => ({
   type: ADD_NOTE,
   note: note
 });
 
-export interface DelNoteAction extends Action {
-  id: number
-}
-export const DEL_NOTE: string = 'DEL_NOTE';
 export const delNote: ActionCreator<Action> = (id: number) => ({
   type: DEL_NOTE,
   id: id
 });
 
-export interface UpdateActiveNoteAction extends Action {
-  note: Note
-}
-export const UPDATE_ACTIVE_NOTE: string = 'UPDATE_ACTIVE_NOTE';
 export const updateActiveNote: ActionCreator<Action> = (note: Note) => ({
   type: UPDATE_ACTIVE_NOTE,
   note: note
 });
 
-
-export interface ToggleShowAction extends Action {
-  show: string;
-}
-export const TOGGLE_SHOW: string = 'TOGGLE_SHOW';
 export const toggleShow: ActionCreator<Action> = (show: string) => ({
   type: TOGGLE_SHOW,
   show: show
 });
 
-export interface ToggleNoteFavoriteAction extends Action {
-  note: Note
-}
-export const TOGGLE_NOTE_FAVORITE = 'TOGGLE_NOTE_FAVORITE';
 export const toggleNoteFavorite: ActionCreator<Action> = (note: Note) => ({
   type: TOGGLE_NOTE_FAVORITE,
   note: note
--- src/app/app-redux/note-reducer.ts
+++ src/app/app-redux/note-reducer.ts
@@ -12,41 +12,58 @@ let initialState: AppState = {
   show: 'all'
 };
 
-export const noteReducer: Reducer<AppState> = (state: AppState = initialState, action: Action) => {
-  switch (action.type) {
-    case NoteActions.ADD_NOTE:
-      return Object.assign({}, state, {
-        notes: state.notes.concat((<NoteActions.AddNoteAction>action).note)
-      });
-    case NoteActions.DEL_NOTE:
-      return Object.assign({}, state, {
-        notes: state.notes.filter((note) => note.id !== (<NoteActions.DelNoteAction>action).id)
-      });
-    case NoteActions.UPDATE_ACTIVE_NOTE:
-      return Object.assign({}, state, {
-        activeNote: (<NoteActions.UpdateActiveNoteAction>action).note
-      });
-    case NoteActions.TOGGLE_SHOW:
-      return Object.assign({}, state, {
-        show: (<NoteActions.ToggleShowAction>action).show
-      })
-    case NoteActions.TOGGLE_NOTE_FAVORITE:
-      // 获取数组副本
-      let oldNotes = state.notes.slice();
-      let newNote = (<NoteActions.ToggleNoteFavoriteAction>action).note;
-      let activeNote: Note;
-      oldNotes.forEach((note) => {
-        if (note == newNote) {
-          note.favorite = !note.favorite
-          activeNote = note;
-          return;
-        }
-      });
-      return Object.assign({}, state, {
-         notes: oldNotes,
-         activeNote: activeNote
-      });
-    default:
+// 构建 reducer 辅助方法
+let createReducer =  (initialState, handlers) => {
+  return (state = initialState, action) => {
+    if (handlers.hasOwnProperty(action.type)) {
+      return handlers[action.type](state, action);
+    } else {
       return state;
+    }
   }
 }
+
+// 所有的 action 类型方法，返回一个键值映射对象
+let reducerHandler = () => ({
+  [NoteActions.ADD_NOTE]: (state, action) => {
+    return Object.assign({}, state, {
+      notes: state.notes.concat((<NoteActions.CommonStateAction>action).note)
+    });
+  },
+  [NoteActions.DEL_NOTE]: (state, action) => {
+    console.log(state);
+    console.log((<NoteActions.CommonStateAction>action).id);
+    return Object.assign({}, state, {
+      notes: state.notes.filter((note) => note.id !== (<NoteActions.CommonStateAction>action).id)
+    });
+  },
+  [NoteActions.UPDATE_ACTIVE_NOTE]: (state, action) => {
+    return Object.assign({}, state, {
+      activeNote: (<NoteActions.CommonStateAction>action).note
+    });
+  },
+  [NoteActions.TOGGLE_SHOW]: (state, action) => {
+    return Object.assign({}, state, {
+      show: (<NoteActions.CommonStateAction>action).show
+    });
+  },
+  [NoteActions.TOGGLE_NOTE_FAVORITE]: (state, action) => {
+    // 获取数组副本
+    let oldNotes = state.notes.slice();
+    let newNote = (<NoteActions.CommonStateAction>action).note;
+    let activeNote: Note;
+    oldNotes.forEach((note) => {
+      if (note == newNote) {
+        note.favorite = !note.favorite
+        activeNote = note;
+        return;
+      }
+    });
+    return Object.assign({}, state, {
+      notes: oldNotes,
+      activeNote: activeNote
+    });
+  }
+});
+
+export const noteReducer: Reducer<AppState> = createReducer(initialState, reducerHandler());
